37dd784cc14be53622c1316bff013cf21e41698f
onicue <onicue@proton.me>
2024-05-30 15:58:44 +0200

    refactoring

--- tests/utils.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-bool checkValidityRequest(std::string request);
-
-template<typename T, typename... Args>
-void assertEquals(T first, Args... args);
-
-void handleError(const std::string& errorMessage);
